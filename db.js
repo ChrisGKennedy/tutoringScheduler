4af0ff824861c8c4ca7915dd25e74295372de221
Christian Kennedy <christiangkennedy@gmail.com>
2021-11-04 18:21:33 -0500

    set up repo

---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgress",
+    password: "",
+    host: "localhost",
+    port: 5432,
+    database: "scheduler"
+});
+
+module.exports = pool;
