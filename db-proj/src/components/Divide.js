13cf3f81efc4989597ff8a7272c1c7c83ef90e15
Brendan Potter <brendanpotter00@gmail.com>
2021-12-06 20:31:35 -0600

    fixing complex function

---
+++ db-proj/src/components/Divide.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./Divide.css";
+
+function Divide() {
+  return (
+    <Fragment>
+      <div className="bar"> d</div>
+    </Fragment>
+  );
+}
+
+export default Divide;
